21ab51bb265aa7045d54fa38a6be61724165a1d2
thanzilhussain <thanzilhussainracer@gmail.com>
2025-12-15 23:43:52 +0530

    Add files via upload

---
+++ d_flipflop.v
@@ -0,0 +1,12 @@
+module d_flipflop (
+    input  wire clk, rst, D,
+    output reg  Q
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst)
+            Q <= 1'b0;   // Reset
+        else
+            Q <= D;      // Capture D at clock edge
+    end
+endmodule
+
